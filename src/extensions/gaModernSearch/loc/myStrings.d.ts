c0abdaa69373d147bcc650d29f176dfea4301b10
Gosia Borzecka <gosia.borzecka@id-live.com>
2020-03-04 10:24:06 +0000

    Initial commit

---
+++ src/extensions/gaModernSearch/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IGaModernSearchApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'GaModernSearchApplicationCustomizerStrings' {
+  const strings: IGaModernSearchApplicationCustomizerStrings;
+  export = strings;
+}
